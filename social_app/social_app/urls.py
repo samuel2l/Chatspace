c79ef64cf1b3498d091d65ec34ec4103cae3b8e5
Samuel Adams <sama29571@gmail.com>
2024-09-09 21:00:42 +0000

    add user signup

--- social_app/social_app/urls.py
+++ social_app/social_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('', include('users.urls')),
+    path('user/', include('users.urls')),
 ]
