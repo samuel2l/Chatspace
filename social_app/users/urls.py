8e3cbfb1381671e249a48a32a3967c2251b41f13
Samuel Adams <sama29571@gmail.com>
2024-09-09 23:42:37 +0000

    add login and out features

--- social_app/users/urls.py
+++ social_app/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 urlpatterns = [
     path('', views.signup,name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="logout.html"),name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"),name='login'),
 
 ]
