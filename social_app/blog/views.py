f944c0a883465f81399cc6f0b4e968358ee454ea
Samuel Adams <sama29571@gmail.com>
2024-09-09 15:50:54 +0000

    add style for posts

--- social_app/blog/views.py
+++ social_app/blog/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+    return render(request,"home.html",{'posts':posts})
 
 def about(request):
     return render(request,"about.html")
