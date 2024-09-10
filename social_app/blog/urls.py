85cedbb611393c6d3f93cbe42029f32f16aac06c
Samuel Adams <sama29571@gmail.com>
2024-09-10 13:53:47 +0000

    add post details view

--- social_app/blog/urls.py
+++ social_app/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 
 from . import views
 urlpatterns = [
-    path('', views.home,name='blog-home'),
+    path('', views.HomeView.as_view(),name='blog-home'),
+    path('post/<int:pk>', views.PostDetailsView.as_view(),name='post-detail'),    
     path('about/', views.about,name='blog-about'),
 ]
--- social_app/blog/views.py
+++ social_app/blog/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
-
+from django.views.generic import ListView,DetailView
 def home(request):
     posts=Post.objects.all()
     if not posts.exists():
@@ -12,9 +12,20 @@ def home(request):
             'author':''
         }]
 
-    
-
     return render(request,"home.html",{'posts':posts})
+#doing the home view as a class based view
+class HomeView(ListView):
+    model=Post
+    template_name='home.html'
+    context_object_name='posts'#defines the name of the context you are sending to your template. by default it is called object
+    ordering=['-date'] #field on which to order results. -date means date in descending order
+
+class PostDetailsView(DetailView):
+    model=Post
+    template_name='post_details.html'
+#if you do not specify a template name it means your template  should follow the convention: template/appname/model_viewtype.html
+
+
 
 def about(request):
     return render(request,"about.html")
